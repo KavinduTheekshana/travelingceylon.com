9032e5227e51ac6e1cf3676aa2ac51f332111907
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-01-05 10:06:50 +0000

    Remove Slider Slide

--- resources/views/frontend/homepage/banner.blade.php
+++ resources/views/frontend/homepage/banner.blade.php
@@ -1,67 +1,74 @@
   <!-- ***home banner html start form here*** -->
   <section class="home-banner-section home-banner-slider">
-   <div class="home-banner d-flex flex-wrap align-items-center" style="background-image: url({{url('frontend/assets/images/slider/the-orphanage-was-founded.webp')}});">
+      <div class="home-banner d-flex flex-wrap align-items-center"
+          style="background-image: url({{ url('frontend/assets/images/slider/the-orphanage-was-founded.webp') }});">
 
-       <div class="overlay"></div>
-       <div class="container header-title">
-          <div class="banner-content text-center">
-             <div class="row">
-                <div class="col-lg-12">
-                   <h2 class="banner-title">Encounter the Majesty of Ceylon's Wildlife</h2>
-                </div>
-             </div>
+          <div class="overlay"></div>
+          <div class="container header-title">
+              <div class="banner-content text-center">
+                  <div class="row">
+                      <div class="col-lg-12">
+                          <h2 class="banner-title">Encounter the Majesty of Ceylon's Wildlife</h2>
+                      </div>
+                  </div>
+              </div>
           </div>
-       </div>
-    </div>
-    <div class="home-banner d-flex flex-wrap align-items-center" style="background-image: url({{url('frontend/assets/images/Leopard-srilanka-Getty.webp')}});">
-       <div class="overlay"></div>
-       <div class="container header-title">
-          <div class="banner-content text-center">
-             <div class="row">
-                <div class="col-lg-12">
-                   <h2 class="banner-title">Discover the Untamed Beauty of Ceylon</h2>
-                </div>
-             </div>
+      </div>
+      <div class="home-banner d-flex flex-wrap align-items-center"
+          style="background-image: url({{ url('frontend/assets/images/Leopard-srilanka-Getty.webp') }});">
+          <div class="overlay"></div>
+          <div class="container header-title">
+              <div class="banner-content text-center">
+                  <div class="row">
+                      <div class="col-lg-12">
+                          <h2 class="banner-title">Discover the Untamed Beauty of Ceylon</h2>
+                      </div>
+                  </div>
+              </div>
           </div>
-       </div>
-    </div>
-
-    <div class="home-banner d-flex flex-wrap align-items-center" style="background-image: url({{url('frontend/assets/images/slider/DDD-Srilanka-1.webp')}});">
-      <div class="overlay"></div>
-      <div class="container header-title">
-         <div class="banner-content text-center">
-      <div class="row">
-         <div class="col-lg-12">
-            <h2 class="banner-title">Marvel at Sri Lanka's Lion Rock, a treasure of the ancient world.</h2>
-         </div>
-      </div></div>
       </div>
-   </div>
 
-   <div class="home-banner d-flex flex-wrap align-items-center" style="background-image: url({{url('frontend/assets/images/slider/GettyRF_1129567869.webp')}});">
-      <div class="overlay"></div>
-      <div class="container header-title">
-         <div class="banner-content text-center">
-      <div class="row">
-         <div class="col-lg-12">
-            <h2 class="banner-title">Unwind on Ceylon's Golden Shores</h2>
-         </div>
+      <div class="home-banner d-flex flex-wrap align-items-center"
+          style="background-image: url({{ url('frontend/assets/images/slider/DDD-Srilanka-1.webp') }});">
+          <div class="overlay"></div>
+          <div class="container header-title">
+              <div class="banner-content text-center">
+                  <div class="row">
+                      <div class="col-lg-12">
+                          <h2 class="banner-title">Marvel at Sri Lanka's Lion Rock, a treasure of the ancient world.
+                          </h2>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
-   </div>
-</div>
-   </div>
 
-   <div class="home-banner d-flex flex-wrap align-items-center" style="background-image: url({{url('frontend/assets/images/slider/merlin.webp')}});">
-      <div class="overlay"></div>
-      <div class="container header-title">
-         <div class="banner-content text-center">
-      <div class="row">
-         <div class="col-lg-12">
-            <h2 class="banner-title">Step into a World of Tranquility in Sri Lanka</h2>
-         </div>
+      <div class="home-banner d-flex flex-wrap align-items-center"
+          style="background-image: url({{ url('frontend/assets/images/slider/GettyRF_1129567869.webp') }});">
+          <div class="overlay"></div>
+          <div class="container header-title">
+              <div class="banner-content text-center">
+                  <div class="row">
+                      <div class="col-lg-12">
+                          <h2 class="banner-title">Unwind on Ceylon's Golden Shores</h2>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
-   </div>
-</div>
-   </div>
- </section>
- <!-- ***home banner html end here*** -->
+
+      {{-- <div class="home-banner d-flex flex-wrap align-items-center"
+          style="background-image: url({{ url('frontend/assets/images/slider/merlin.webp') }});">
+          <div class="overlay"></div>
+          <div class="container header-title">
+              <div class="banner-content text-center">
+                  <div class="row">
+                      <div class="col-lg-12">
+                          <h2 class="banner-title">Step into a World of Tranquility in Sri Lanka</h2>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div> --}}
+  </section>
+  <!-- ***home banner html end here*** -->
