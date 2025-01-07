2c0181a6a0d5f70bee7157ac86319334efc23826
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-01-07 21:20:42 +0000

    Footer A Tag

--- resources/views/frontend/components/footer.blade.php
+++ resources/views/frontend/components/footer.blade.php
@@ -8,7 +8,7 @@
                             <div class="footer-logo">
                                 <a href="{{ url('/') }}"><img loading="lazy"
                                         src="{{ asset('frontend/assets/images/logo/travelingceylon.svg') }}"
-                                        alt=""></a>
+                                        alt="travelingceylon logo"></a>
                             </div>
                             <div class="textwidget widget-text">
                                 Welcome to our travel website! With over 22 years of experience in organizing
@@ -42,12 +42,12 @@
                                             <a class="footer-image"
                                                 href="{{ route('packages.single', ['slug' => $package->slug]) }}"><img loading="lazy"
                                                     class="footer-image" src="{{ asset($package->image) }}"
-                                                    alt=""></a>
+                                                    alt="{{$package->title}}"></a>
                                         </figure>
                                         <div class="post-content">
                                             <h6>
                                                 <a
-                                                    href="{{ route('packages.single', ['slug' => $package->slug]) }}">{{ Str::limit(strip_tags($package->title), 60, '...') }}</a>
+                                                    href="{{ route('packages.single', ['slug' => $package->slug]) }}" area-label="{{$package->title}}">{{ Str::limit(strip_tags($package->title), 60, '...') }}</a>
                                             </h6>
 
                                         </div>
@@ -92,7 +92,7 @@
                             <div class="gallery gallery-colum-3">
 
                                 @foreach($destinations_list as $key => $destination)
-                                <a class="footer-primary-text" href="{{ url('/destination/' . $destination->slug) }}">{{ $destination->title }}</a>@if(!$loop->last),&nbsp; @endif
+                                <a class="footer-primary-text" href="{{ url('/destination/' . $destination->slug) }}" area-label="{{ $destination->title }}">{{ $destination->title }}</a>@if(!$loop->last),&nbsp; @endif
                             @endforeach
 
                                 {{-- @foreach ($gallery_footer as $gallery)
@@ -108,69 +108,12 @@
                     </div>
                 </div>
             </div>
-            {{-- <div class="lower-footer">
-             <div class="row align-items-center">
-                <div class="col-lg-6">
-                   <div class="footer-newsletter">
-                      <p>Subscribe our newsletter for more update & news !!</p>
-                      <form class="newsletter">
-                         <input type="email" name="email" placeholder="Enter Your Email">
-                         <button type="submit" class="outline-btn outline-btn-white">Subscribe</button>
-                      </form>
-                   </div>
-                </div>
-                <div class="col-lg-6 text-right">
-                   <div class="social-icon">
-                      <ul>
-                         <li>
-                            <a href="https://www.facebook.com/" target="_blank">
-                               <i class="fab fa-facebook-f" aria-hidden="true"></i>
-                            </a>
-                         </li>
-                         <li>
-                            <a href="https://www.twitter.com/" target="_blank">
-                               <i class="fab fa-twitter" aria-hidden="true"></i>
-                            </a>
-                         </li>
-                         <li>
-                            <a href="https://www.youtube.com/" target="_blank">
-                               <i class="fab fa-youtube" aria-hidden="true"></i>
-                            </a>
-                         </li>
-                         <li>
-                            <a href="https://www.instagram.com/" target="_blank">
-                               <i class="fab fa-instagram" aria-hidden="true"></i>
-                            </a>
-                         </li>
-                         <li>
-                            <a href="https://www.linkedin.com/" target="_blank">
-                               <i class="fab fa-linkedin" aria-hidden="true"></i>
-                            </a>
-                         </li>
-                      </ul>
-                   </div>
-                   <div class="footer-menu">
-                      <ul>
-                         <li>
-                            <a href="policy.html">Privacy Policy</a>
-                         </li>
-                         <li>
-                            <a href="policy.html">Term & Condition</a>
-                         </li>
-                         <li>
-                            <a href="faq.html">FAQ</a>
-                         </li>
-                      </ul>
-                   </div>
-                </div>
-             </div>
-          </div> --}}
         </div>
     </div>
     <div class="bottom-footer">
         <div class="container">
             <div class="copy-right text-center">Copyright &copy; {{ now()->year }} Traveling Ceylon | Developed By:
-                <a href="https://neuroon.lk" target="_blank">Neuroon Informatics</a>
+                <a href="https://neuroon.lk" area-label="Neuroon Informatics" target="_blank">Neuroon Informatics</a>
             </div>
         </div>
     </div>
